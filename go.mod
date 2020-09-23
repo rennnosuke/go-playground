8f2e73e14ad8b1233589a6673cbcb1ca66f29062
RenKanai <turutekab@gmail.com>
2020-09-23 23:39:34 +0900

    add websocket

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1
+	github.com/aws/aws-sdk-go v1.33.5 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
 )
---
+++ websocket/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	http.Handle("/echo", websocket.Handler(echoHandler))
+	if err := http.ListenAndServe("localhost:8000", nil); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func echoHandler(c *websocket.Conn) {
+	if _, err := io.Copy(c, c); err != nil {
+		log.Fatalln(err)
+	}
+}
