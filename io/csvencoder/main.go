907e054c68a0e80854241a659569d9b9f91db047
RenKanai <turutekab@gmail.com>
2021-02-28 16:47:58 +0900

    add io/csvencoder

--- io/csvencoder/main.go
+++ io/csvencoder/main.go
@@ -1,1 +1,14 @@
-package csvencoder
+package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func main() {
+	encoder := csv.NewWriter(os.Stdout)
+	defer encoder.Flush()
+	if err := encoder.Write([]string{"write line"}); err != nil {
+		panic(err)
+	}
+}
