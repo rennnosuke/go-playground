107a961de1d3784bc7308d7d89eb626e8b2ccc8b
RenKanai <rk2904powr@gmail.com>
2022-07-30 00:20:29 +0900

    modify slice/slice_test.go

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -29,8 +29,33 @@ func TestSliceTrick(t *testing.T) {
 	a = []int{1, 2, 3, 4, 5}
 	fmt.Println(append(a[:i], a[i+1:]...)) // [1 2 4 5]
 
+	// insert a slice to another one: part1
 	a = []int{1, 2, 3, 4, 5}
 	a = a[:i+copy(a[i:], a[i+1:])]
 	fmt.Println(a) // [1 2 4 5]
 
+	// insert a slice to another one: part2
+	a = []int{10, 20, 30}
+	b = []int{88, 99}
+	i = 2
+	fmt.Println(append(a[:i], append(b, a[i:]...)...))
+
+	// push
+	a = []int{10, 20, 30}
+	fmt.Println(append(a, 40))
+
+	// pop
+	a = []int{10, 20, 30}
+	x, a := a[len(a)-1], a[:len(a)-1]
+	fmt.Println(x, a)
+
+	// push front
+	a = []int{10, 20, 30}
+	x = 0
+	fmt.Println(append([]int{x}, a...))
+
+	// unshift
+	a = []int{10, 20, 30}
+	x, a = a[0], a[1:]
+	fmt.Println(x, a)
 }
