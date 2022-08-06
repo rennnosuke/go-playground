9f9cd62ecd31b6f76a7d3e8678008b2f905e6ceb
RenKanai <rk2904powr@gmail.com>
2022-08-06 23:48:23 +0900

    add first test to squirrel/main_test.go

--- squirrel/main_test.go
+++ squirrel/main_test.go
@@ -1,8 +1,28 @@
 package squirrel
 
-import "testing"
+import (
+	"testing"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestSquirrel(t *testing.T) {
 	t.Log("start squirrel examples")
+
+	// select
+	q, _, err := sq.Select(
+		"*",
+	).From(
+		"users",
+	).Where(sq.Eq{"id": 1}).ToSql()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "SELECT * FROM users WHERE id = ?"
+	t.Log(want)
+	if diff := cmp.Diff(want, q); diff != "" {
+		t.Errorf("MyFunc() mismatch (-want +got):\n%s", diff)
+	}
 
 }
