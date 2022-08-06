7f9523c560235d8c78ab1777c0053bb8d9330a4f
RenKanai <rk2904powr@gmail.com>
2022-08-06 23:56:44 +0900

    modify squirrel/main_test.go: add table tests

--- squirrel/main_test.go
+++ squirrel/main_test.go
@@ -7,22 +7,37 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func hoge() {
+
+}
+
 func TestSquirrel(t *testing.T) {
 	t.Log("start squirrel examples")
 
-	// select
-	q, _, err := sq.Select(
-		"*",
-	).From(
-		"users",
-	).Where(sq.Eq{"id": 1}).ToSql()
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name    string
+		builder interface {
+			ToSql() (string, []interface{}, error)
+		}
+		want string
+	}{
+		{
+			name:    "select#1",
+			builder: sq.Select("*").From("users").Where(sq.Eq{"id": 1}),
+			want:    "SELECT * FROM users WHERE id = ?",
+		},
 	}
-	want := "SELECT * FROM users WHERE id = ?"
-	t.Log(want)
-	if diff := cmp.Diff(want, q); diff != "" {
-		t.Errorf("MyFunc() mismatch (-want +got):\n%s", diff)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, args, err := tt.builder.ToSql()
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("query: %s", got)
+			t.Logf("args: %v", args)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("squirrel: built sql mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
-
 }
