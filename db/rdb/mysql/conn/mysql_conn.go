d712b709da263317c8d5e1a9f25bff8edb6ebbfb
RenKanai <6196911+rennnosuke@users.noreply.github.com>
2024-10-06 00:38:58 +0900

    rm mysqlConfig

--- db/rdb/mysql/conn/mysql_conn.go
+++ db/rdb/mysql/conn/mysql_conn.go
@@ -9,16 +9,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type mySQLConfig struct {
-	userName string
-	password string
-	dbName   string
-}
-
-func (c mySQLConfig) getDataSourceName() string {
-	return c.userName + ":" + c.password + "@/" + c.dbName
-}
-
 func open(_ context.Context, conf mysql.Config) (*sql.DB, error) {
 	db, err := sql.Open("mysql", conf.FormatDSN())
 	if err != nil {
